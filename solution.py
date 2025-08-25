1abfed0c31fae32ce80b50a3ca24a9e775c6fbaf
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-08-25 19:04:37 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        return sum(stone in jewels for stone in stones)
